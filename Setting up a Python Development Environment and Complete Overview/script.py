8dd83154a99312735242fff2bda742ed8f489459
sonicprocess <sonicprocess@gmail.com>
2020-04-25 11:27:32 +0200

    Removed some code

--- Setting up a Python Development Environment and Complete Overview/script.py
+++ Setting up a Python Development Environment and Complete Overview/script.py
@@ -1,23 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-name = input('Your name? ')
-print('Hello,', name)
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-# print(greet('World'))
-# print(greet('Corey'))
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
